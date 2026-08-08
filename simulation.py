bb84273090f8dcbb9ed768eda9ae073c1f0d0710
SumeyaNUR <sumeyanur81@gmail.com>
2026-08-08 15:55:12 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -62,6 +62,8 @@ def simulate_transmission(payload_bytes, loss_rate=LOSS_PROBABILITY):
 # Finite-field Diffie-Hellman
 
 def run_ffdh():
+        """Run one finite-field Diffie-Hellman key exchange simulation."""
+
     start = time.perf_counter()
 
     private_key_a = DH_PARAMETERS.generate_private_key()
@@ -118,6 +120,7 @@ def run_ffdh():
 # Elliptic Curve Diffie-Hellman
 
 def run_ecdh():
+        """Run one elliptic-curve Diffie-Hellman key exchange simulation."""
     start = time.perf_counter()
 
     private_key_a = ec.generate_private_key(ec.SECP256R1())
